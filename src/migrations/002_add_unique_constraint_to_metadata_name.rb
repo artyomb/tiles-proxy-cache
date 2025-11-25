5c448b4959e27f33281660e253e77ab0097d8de6
Alexander Ludov <efraam@gmail.com>
2025-11-25 19:43:29 +0600

    fix: fixed LocalJumpError in migration 002

--- src/migrations/002_add_unique_constraint_to_metadata_name.rb
+++ src/migrations/002_add_unique_constraint_to_metadata_name.rb
@@ -1,13 +1,13 @@
 Sequel.migration do
   up do
-    return unless table_exists?(:metadata)
+    next unless table_exists?(:metadata)
 
     has_unique = begin
       indexes(:metadata).values.any? { |idx| idx[:unique] && idx[:columns] == [:name] }
     rescue
       false
     end
-    return if has_unique
+    next if has_unique
 
     duplicates = self[:metadata].select(:name).group(:name).having { count(:name) > 1 }.all
     raise "Cannot add unique constraint: duplicate names found: #{duplicates.map { |d| d[:name] }.join(', ')}" if duplicates.any?
@@ -23,14 +23,14 @@ Sequel.migration do
   end
 
   down do
-    return unless table_exists?(:metadata)
+    next unless table_exists?(:metadata)
 
     has_unique = begin
       indexes(:metadata).values.any? { |idx| idx[:unique] && idx[:columns] == [:name] }
     rescue
       false
     end
-    return unless has_unique
+    next unless has_unique
 
     create_table(:metadata_old) do
       String :name, null: false
