69c6e241240a2dad8fad7e3f399048ad5985df99
Alexander Ludov <efraam@gmail.com>
2025-12-24 19:42:01 +0600

    Revert "test: PATH_PREFIX in view_helpers.rb"
    This reverts commit 8c108ac996c5759c8161fde8abf0a4bafa0945f2.

--- src/view_helpers.rb
+++ src/view_helpers.rb
@@ -86,10 +86,7 @@ module ViewHelpers
   end
 
   def generate_single_source_style(route, source_name, debug_mode = false)
-    x_path_prefix = request.env['HTTP_X_FORWARDED_PATH_PREFIX'] || request.env['HTTP_X_FORWARDED_PREFIX'] || ''
-    base_path = request.host_with_port + ENV['PATH_PREFIX'].to_s + x_path_prefix
-    base_url = "#{request.scheme}://#{base_path}"
-    
+    base_url = request.base_url
     encoding = route.dig(:metadata, :encoding)
     is_terrain = %w[terrarium mapbox].include?(encoding)
 
